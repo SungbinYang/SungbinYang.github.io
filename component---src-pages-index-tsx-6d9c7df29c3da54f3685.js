93087af3d65ea020aa05c6808755d2b3612b68e4
SungbinYang <ysb05222@naver.com>
2026-03-22 14:12:22 +0000

    Updates

---
+++ component---src-pages-index-tsx-6d9c7df29c3da54f3685.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk_vallista_land_blog=self.webpackChunk_vallista_land_blog||[]).push([[245],{8871:function(e,t,i){i.r(t);var a=i(8883),r=i(8545),n=i(1687),s=i(7040),l=i(9956),o=i(9018),g=i(3448),d=i(5723);const m=r.i7`
+  0% {
+    transform: rotateZ(0);
+  }
+
+  50% {
+    transform: rotateZ(30deg);
+  }
+
+  100% {
+    transform: rotateZ(0deg);
+  }
+`,h=(0,a.A)("div",{target:"egaugi64"})({name:"8iutvn",styles:"margin-left:auto;margin-right:auto;max-width:900px;padding:2rem"}),u=(0,a.A)("header",{target:"egaugi63"})({name:"1j66vkv",styles:"padding:2rem 0"}),c=(0,a.A)("div",{target:"egaugi62"})("margin-bottom:1.5rem;max-width:550px;&>span:first-of-type{position:relative;&::after{position:absolute;right:-4rem;top:-0.5rem;content:'✋';display:block;animation:",m," 1s ease-in-out infinite;}}"),x=(0,a.A)("div",{target:"egaugi61"})({name:"1i7yo1v",styles:"max-width:550px;margin-bottom:2rem"}),p=(0,a.A)("section",{target:"egaugi60"})({name:"1jwuio6",styles:"width:100%;margin-left:auto;margin-right:auto;max-width:900px;padding:2rem"});t.default=e=>{const{data:t}=e,{nodes:i}=t.allMarkdownRemark;return(0,d.jsxs)(n.mc,{children:[(0,d.jsx)(o.G,{name:"홈"}),(0,d.jsx)(u,{children:(0,d.jsxs)(h,{children:[(0,d.jsxs)(c,{children:[(0,d.jsx)(n.EY,{as:"span",size:48,weight:800,children:"어서오세요!"}),(0,d.jsx)(n.EY,{as:"h1",size:48,weight:800,lineHeight:56,children:"저는 양성빈입니다."})]}),(0,d.jsx)(x,{children:(0,d.jsxs)(n.EY,{as:"p",size:20,weight:400,lineHeight:40,children:["대한민국 서울에서 ",(0,d.jsx)("strong",{children:"소프트웨어 엔지니어"}),"로 일하고 있어요. 개발을 접해보기 전부터 몰입하는 즐거움을 따라 살다가, 지금은 개발에 푹 빠져 있습니다. 항상 무언가를 배우는데 있어서 매일 같이 꾸준히 공부를 하고 있습니다. 함께 발전 및 성장해나가는 것을 즐겨합니다."]})}),(0,d.jsx)(n.$n,{size:"large",color:"alert",onClick:()=>{return e="/posts",void(0,s.navigate)(e);var e},children:(0,d.jsx)(n.EY,{size:16,weight:800,children:"블로그 글 보러갈까요?"})})]})}),(0,d.jsx)(p,{children:(0,d.jsx)(l.V,{title:"최근 글",list:(a=i,(0,g.KF)(a).filter((e,t)=>t<6).map(e=>({name:e.frontmatter.title,slug:e.fields.slug,date:r(e.frontmatter.date)})))})})]});var a;function r(e){const[,t,i]=(0,g.WB)(e);return`${Number(t)}월 ${Number(i)}일`}}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-6d9c7df29c3da54f3685.js.map
